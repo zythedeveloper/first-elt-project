14fea71b891d862bfeeb7ca4f0d895f7902ac536
zythedeveloper <139963492+zythedeveloper@users.noreply.github.com>
2025-01-18 00:12:51 +0800

    Use dbt for data transformations

---
+++ transforms/target/run/transforms/models/example/film_ratings.sql
@@ -0,0 +1,41 @@
+
+  
+    
+
+  create  table "destination_db"."public"."film_ratings__dbt_tmp"
+  as (
+    WITH films_with_ratings AS (
+    SELECT
+        film_id,
+        title,
+        release_date,
+        price,
+        rating,
+        user_rating,
+        CASE
+            WHEN user_rating >= 4.5 THEN 'Excellent'
+            WHEN user_rating >= 4.0 THEN 'Good'
+            WHEN user_rating >= 3.0 THEN 'Average'
+            ELSE 'Poor'
+        END AS rating_category
+    FROM "destination_db"."public"."films"  -- Using dbt's ref function to reference the films table
+),
+
+films_with_actors AS (
+    SELECT
+        f.film_id,
+        f.title,
+        STRING_AGG(a.actor_name, ', ') AS actors  -- Aggregating actor names for each film
+    FROM "destination_db"."public"."films" f
+    LEFT JOIN "destination_db"."public"."film_actors" fa ON f.film_id = fa.film_id
+    LEFT JOIN "destination_db"."public"."actors" a ON fa.actor_id = a.actor_id
+    GROUP BY f.film_id, f.title
+)
+
+SELECT
+    fwf.*,
+    fwa.actors
+FROM films_with_ratings fwf
+LEFT JOIN films_with_actors fwa ON fwf.film_id = fwa.film_id
+  );
+  
